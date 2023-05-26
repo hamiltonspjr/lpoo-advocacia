de769f1311a044ca6dfa6f873e4ed3bbc49cfefb
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-05-25 22:12:25 -0300

    initial commit

---
+++ src/test/java/br/edu/ifsul/cstsi/advocacia/AdvocaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.cstsi.advocacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvocaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
